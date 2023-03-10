6e1d22f79e71f742205e41c7c44e08cc9398163d
suatt-prog <72493647+suatt-prog@users.noreply.github.com>
2023-03-10 14:59:01 +0300

    Update urls.py

--- website/main/urls.py
+++ website/main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns =[path("<int:id>",views.index,name="index"),
               path("v1",views.v1,name="page 2"),
-              path("home",views.home,name="home")
+              path("home",views.home,name="home"),
+              path("",views.home,name="home")
               ]
